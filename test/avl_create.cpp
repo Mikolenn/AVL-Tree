eeea952a0e8edd83ad0dd4c4436a39b2e775c8d1
Mikolenn <castroariasricardo@gmail.com>
2020-10-04 18:30:20 -0600

    CMake now is working

--- test/avl_create.cpp
+++ test/avl_create.cpp
@@ -1,5 +1,6 @@
-#include "functions.h"
-#include "gtest/gtest.h"
+#include <functions.h>
+#include <gtest/gtest.h>
+#include <time.h>
 
 TEST(avlcreate, positive){
 
@@ -13,7 +14,21 @@ TEST(avlcreate, positive){
     EXPECT_EQ(status, 0);
 }
 
+TEST(avlcreate, negative){
+
+    int status = 0;
+
+    Node* root=NULL;
+    float arr[]= {1,1,2,3,4,5};
+
+    status = avl_create(arr, &root);
+
+    EXPECT_EQ(status, -1);
+}
+
 int main(int argc, char **argv){
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
+
+    return 0;
 }
